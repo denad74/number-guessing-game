ee473cf40a7a26038a1263191d68f33cd35f1a45
Denad74 <denad.ramic@gmail.com>
2021-11-03 09:12:19 +0100

    Number Guessing Game with ReactJs

--- README.md
+++ README.md
@@ -1,70 +1,13 @@
-# Getting Started with Create React App
+Number Guessing game
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Number Guessing Gamep is local standalone rect project with the server bound to the port 3000. It's single page application. How to use NGG:
 
-## Available Scripts
+The Number Guessing game is game where the user guesses a randomly genereted number from 1 to 100. User have to enter a number and click on the "Submit number". Max number of attempts is 10. If user etered number that is low or hight than secret number he get massage in alert fild. If user guess the secret number he get message 'Congratulations! You got it right!'.Clear button slears the entered value. Reset button resets game at the beginning. To start new game user have to click on button 'Start New Game'
 
-In the project directory, you can run:
+Tehnologies used to build the game:
+HTML,
+CSS,
+JS,
+ReactJS
 
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+To start app use npm start command in terminal. React app is run on http://localhost:3000
--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [randomNumber, setRandomNumber] = useState(
+    Math.ceil(Math.random() * 100)
+  );
+  const [answerNumber, setAnswerNumber] = useState("");
+  const [attempts, setAttempts] = useState(10);
+  const [prevAnswer, setPrevAnswer] = useState([]);
+  const [messageAlert, setMessageAlert] = useState("");
+  const [isStart, setIsStart] = useState(false);
+  const [isOver, setIsOver] = useState(false);
+  const [styleClass, setStyleClass] = useState("");
+  const [isdisable, setIsdisable] = useState(false);
+
+  console.log(randomNumber);
+
+  const submit = (event) => {
+    event.preventDefault();
+
+    if (answerNumber.trim() !== "") {
+      if (!prevAnswer.length === 0 || prevAnswer.includes(answerNumber)) {
+        return alert("You have already entered this number! Try again!");
+      } else {
+        if (attempts !== 1) {
+          if (Number(answerNumber) === randomNumber) {
+            setMessageAlert("Congratulations! You got it right!");
+            setStyleClass("bg-success");
+            setIsStart(true);
+            setIsOver(true);
+            setIsdisable(true);
+            setAnswerNumber("");
+          } else if (answerNumber > randomNumber) {
+            setMessageAlert("PS! The last guess was too high!");
+            setStyleClass("bg-denger");
+            setAttempts(attempts - 1);
+            setIsStart(true);
+          } else if (answerNumber < randomNumber) {
+            setMessageAlert("UPS! The last guess was too low");
+            setStyleClass("bg-info");
+            setAttempts(attempts - 1);
+            setIsStart(true);
+          }
+        } else {
+          setIsOver(true);
+          setMessageAlert("GAME OVER!");
+          setStyleClass("bg-warning");
+          setIsStart(true);
+          setPrevAnswer([]);
+          setIsdisable(true);
+        }
+      }
+      setPrevAnswer([...prevAnswer, answerNumber]);
+      setAnswerNumber("");
+    }
+  };
+
+  function handleChange(e) {
+    e.preventDefault();
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === "" || re.test(e.target.value)) {
+      setAnswerNumber(e.target.value);
+
+      console.log(e.target.value);
+    } else {
+      alert("please check your phone number");
+    }
+  }
+
+  function startNewGame() {
+    setRandomNumber(Math.ceil(Math.random() * 100));
+    setAttempts(10);
+    setPrevAnswer([]);
+    setStyleClass("");
+    setIsStart(false);
+    setIsOver(false);
+    setIsdisable(false);
+    setAnswerNumber("");
+    setMessageAlert("");
+  }
+
+  function reset() {
+    if (!isOver) {
+      setAttempts(10);
+      setPrevAnswer([]);
+      setStyleClass("");
+      setMessageAlert("");
+      setAnswerNumber("");
+    }
+  }
+
+  function clear() {
+    setAnswerNumber("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Number Guessing Game</h1>
+      <div className="container">
+        <form onSubmit={submit}>
+          <label htmlFor="number">Enter a number</label>
+          <input type="number" onChange={handleChange} value={answerNumber} />
+        </form>
+        <button className="btn-big" onClick={submit} disabled={isdisable}>
+          Submit number
+        </button>
+        <button className="btn-small" onClick={clear}>
+          Clear
+        </button>
+        <button className="btn-small" onClick={reset}>
+          Reset
+        </button>
+
+        <hr />
+        <div
+          className="attempts-box"
+          style={{ visibility: isStart ? "visible" : "hidden" }}
         >
-          Learn React
-        </a>
-      </header>
+          <p>Remaining attempts: {attempts}</p>
+        </div>
+        <div style={{ visibility: isStart ? "visible" : "hidden" }}>
+          <p>Previous guesses: {prevAnswer + ","} </p>
+        </div>
+        <div
+          style={{
+            visibility: isStart ? "visible" : "hidden",
+          }}
+          className={styleClass}
+        >
+          <p style={{ padding: "10px", color: "#fff" }}>{messageAlert}</p>
+        </div>
+        <div>
+          <button
+            style={{ visibility: isOver ? "visible" : "hidden" }}
+            className="btn-big"
+            onClick={startNewGame}
+          >
+            Star new game
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
